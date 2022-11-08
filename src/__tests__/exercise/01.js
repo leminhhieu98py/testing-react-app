71a0655668461137f6d3befe4c31f33ed2e21ee0
otis <leminhhieu98py@gmail.com>
2022-11-08 16:36:21 +0700

    refactor

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -36,15 +36,29 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const root = createRoot(div)
   act(() => root.render(<Counter />))
 
+  const increaseClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+
+  const decreaseClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+
   const buttons = document.querySelectorAll('button')
   const increment = buttons[0]
   const decrement = buttons[1]
   const message = div.firstChild.querySelector('div')
 
   expect(message).toHaveTextContent('0')
-  act(() => increment.click())
+
+  act(() => increment.dispatchEvent(increaseClickEvent))
   expect(message).toHaveTextContent('1')
-  act(() => decrement.click())
+
+  act(() => decrement.dispatchEvent(decreaseClickEvent))
   expect(message).toHaveTextContent('0')
 
   root.unmount()
