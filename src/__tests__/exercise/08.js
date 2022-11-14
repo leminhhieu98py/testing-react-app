ec270c3481a6fa44c16b131ebb859c59ec73d3e1
otis <leminhhieu98py@gmail.com>
2022-11-14 15:47:38 +0700

    8 - extra 3

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,7 +2,7 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render} from '@testing-library/react'
+import {render, renderHook} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 import {act} from '@testing-library/react'
 
@@ -12,62 +12,48 @@ import {act} from '@testing-library/react'
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
 
-const result = {}
-const TestComponent = props => {
-  Object.assign(
-    result,
-    useCounter({
-      initialCount: props?.initialCount || 0,
-      step: props?.step || 1,
-    }),
-  )
-  return null
-}
-
 test('exposes the count and increment/decrement functions', () => {
   // 🐨 render the component
   // 🐨 get the elements you need using screen
   // 🐨 assert on the initial state of the hook
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
-  render(<TestComponent />)
+  const {result} = renderHook(useCounter)
+  const {increment, decrement} = result.current
 
-  const {increment, decrement} = result
-
-  expect(result.count).toBe(0)
+  expect(result.current.count).toBe(0)
 
   act(() => increment())
-  expect(result.count).toBe(1)
+  expect(result.current.count).toBe(1)
 
   act(() => decrement())
-  expect(result.count).toBe(0)
+  expect(result.current.count).toBe(0)
 })
 
 test('allows customization of the initial count', () => {
-  render(<TestComponent initialCount={1} />)
+  const {result} = renderHook(useCounter, {initialProps: {initialCount: 1}})
+  const {increment, decrement} = result.current
 
-  const {increment, decrement} = result
-
-  expect(result.count).toBe(1)
+  expect(result.current.count).toBe(1)
 
   act(() => increment())
-  expect(result.count).toBe(2)
+  expect(result.current.count).toBe(2)
 
   act(() => decrement())
-  expect(result.count).toBe(1)
+  expect(result.current.count).toBe(1)
 })
 
 test('allows customization of the step', () => {
-  render(<TestComponent step={5} />)
+  const {result} = renderHook(useCounter, {initialProps: {step: 5}})
 
-  const {increment, decrement} = result
+  const {increment, decrement} = result.current
 
-  expect(result.count).toBe(0)
+  expect(result.current.count).toBe(0)
 
   act(() => increment())
-  expect(result.count).toBe(5)
+  expect(result.current.count).toBe(5)
 
   act(() => decrement())
-  expect(result.count).toBe(0)
+  expect(result.current.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
